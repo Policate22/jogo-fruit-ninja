240e0284068d8acf618aeaf64b90052aa797423f
policate <jdasilvapolicate@gmail.com>
2025-02-12 17:01:28 -0300

    comiit

--- fruti-ninja/script.js
+++ fruti-ninja/script.js
@@ -9,29 +9,49 @@ canvas.height = window.innerHeight
 document.body.prepend(canvas)
 var ctx = canvas.getContext("2d")
 
-function desenhar(){
+var x = 0
+var y = 0
+var w = 50
+var h = 50
+var p_inicial_x = Math.random() * (canvas.height - 100) + 50
+var p_inicial_y = canvas.height - 50
+var velocidade = 70 
+
+var angulo_radiano = velocidade * (Math.PI / 180) 
+var v_inicial_x = velocidade * Math.cos(angulo_radiano)
+var v_inicial_y = -velocidade * Math.sin(angulo_radiano)
+const g = 9.82
+var tempo = 0
+var escala = 20
+
+
+
+function desenhar(p_tempo){
+
+  tempo += p_tempo * escala
 
 ctx.clearRect(0,0,canvas.width, canvas.height)
 
-var x = Math.random() * window.innerWidth
-var y = Math.random() * window.innerHeight
-
-var w = 50
-var h = 50
+x = p_inicial_x + v_inicial_x * tempo
+y = p_inicial_y + (v_inicial_y * tempo) + (0.5 * g * tempo * tempo)
 
 ctx.beginPath()
 ctx.rect( x , y , w , h )
 ctx.fillStyle = "red"
 ctx.fill()
+
+if(y > canvas.height - 50){
+   tempo = 0}
+
 }
 
 var ultimoTempo = 0
 
 function atualizar(deltaTempo){
-   var time = deltaTempo - ultimoTempo
+   var time = (deltaTempo - ultimoTempo) / 1000
    ultimoTempo = deltaTempo
-   desenhar()
    console.log(time)
+   desenhar(time)
    
    requestAnimationFrame(atualizar)
 }
